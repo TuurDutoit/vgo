dec1ec701cdcb9e5315027a3012ad31bab37ec9b
Tuur Dutoit <me@tuurdutoit.be>
2018-11-16 13:27:25 +0100

    Add stone counter and overall progress bar

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -13,15 +13,20 @@ namespace ViewModel
         public BoardViewModel BoardVM { get; }
         public PlayerViewModel Player1VM { get; }
         public PlayerViewModel Player2VM { get; }
-        private Cell<ReversiGame> cGame { get; }
+        public Cell<int> cTotalStones { get; }
+        public Cell<int> cTotalSquares { get; }
+        private Cell<ReversiGame> _cGame { get; }
+        private Player _Player1 = Player.BLACK;
+        private Player _Player2 = Player.WHITE;
 
         public MainViewModel(int width, int height, String name1, String name2)
         {
-            var game = new ReversiGame(width, height);
-            this.cGame = Cell.Create(game);
-            this.BoardVM = new BoardViewModel(cGame);
-            this.Player1VM = new PlayerViewModel(cGame, Player.BLACK, name1);
-            this.Player2VM = new PlayerViewModel(cGame, Player.WHITE, name2);
+            this._cGame = Cell.Create(new ReversiGame(width, height));
+            this.BoardVM = new BoardViewModel(_cGame);
+            this.Player1VM = new PlayerViewModel(_cGame, Player.BLACK, name1);
+            this.Player2VM = new PlayerViewModel(_cGame, Player.WHITE, name2);
+            this.cTotalStones = _cGame.Derive(game => game.Board.CountStones(_Player1) + game.Board.CountStones(_Player2));
+            this.cTotalSquares = _cGame.Derive(game => game.Board.Width * game.Board.Height);
         }
     }
 }
--- ViewModel/PlayerViewModel.cs
+++ ViewModel/PlayerViewModel.cs
@@ -12,11 +12,15 @@ namespace ViewModel
     {
         public String Name { get; }
         public Player Player { get; }
+        public Cell<int> cStones { get; }
+        public Cell<int> cTotalSquares { get; }
 
         public PlayerViewModel(Cell<ReversiGame> cGame, Player player, String name)
         {
             this.Name = name;
             this.Player = player;
+            this.cStones = cGame.Derive(game => game.Board.CountStones(player));
+            this.cTotalSquares = cGame.Derive(game => game.Board.Width * game.Board.Height);
         }
     }
 }
