836aca01f0c8dbdb16e765f92266fdab8426ed49
Tuur Dutoit <me@tuurdutoit.be>
2018-11-16 11:10:56 +0100

    Finish welcome view

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -11,13 +11,17 @@ namespace ViewModel
     public class MainViewModel
     {
         public BoardViewModel BoardVM { get; }
+        public String Name1 { get; }
+        public String Name2 { get; }
         private Cell<ReversiGame> cGame { get; }
 
-        public MainViewModel(int width, int height)
+        public MainViewModel(int width, int height, String name1, String name2)
         {
             var game = new ReversiGame(width, height);
             this.cGame = Cell.Create(game);
             this.BoardVM = new BoardViewModel(cGame);
+            this.Name1 = name1;
+            this.Name2 = name2;
         }
     }
 }
--- ViewModel/WelcomeViewModel.cs
+++ ViewModel/WelcomeViewModel.cs
@@ -5,15 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Cells;
+using Model.Reversi;
 
 namespace ViewModel
 {
     public class WelcomeViewModel
     {
+        public List<int> Widths { get; }
+        public List<int> Heights { get; }
         public Cell<String> cName1 { get; }
         public Cell<String> cName2 { get; }
-        public Cell<String> cWidth { get; }
-        public Cell<String> cHeight { get; }
+        public Cell<int> cWidth { get; }
+        public Cell<int> cHeight { get; }
         public ICommand GoToScreen { get; }
         private GoToScreenAction _goToScreen;
         private Cell<bool> _canStart;
@@ -21,23 +24,37 @@ namespace ViewModel
 
         public WelcomeViewModel(GoToScreenAction goToScreen)
         {
+            this.Widths = new List<int>();
+            this.Heights = new List<int>();
             this.cName1 = Cell.Create("");
             this.cName2 = Cell.Create("");
-            this.cWidth = Cell.Create("8");
-            this.cHeight = Cell.Create("8");
+            this.cWidth = Cell.Create(8);
+            this.cHeight = Cell.Create(8);
             this._goToScreen = goToScreen;
-            this._canStart = Cell.Derived(cName1, cName2,
-                (name1, name2) => !String.IsNullOrWhiteSpace(name1) && !String.IsNullOrWhiteSpace(name2));
+            this._canStart = Cell.Derived(cName1, cName2, cWidth, cHeight, (name1, name2, w, h) =>
+                !String.IsNullOrWhiteSpace(name1) &&
+                !String.IsNullOrWhiteSpace(name2) &&
+                ReversiBoard.IsValidWidth(w) &&
+                ReversiBoard.IsValidHeight(h)
+            );
             this.GoToScreen = new ConditionalCommand(_canStart, navigate);
+
+            for(int i = 0; i < 30; i++)
+            {
+                if(ReversiBoard.IsValidWidth(i))
+                {
+                    Widths.Add(i);
+                }
+                if(ReversiBoard.IsValidHeight(i))
+                {
+                    Heights.Add(i);
+                }
+            }
         }
 
         private void navigate()
         {
-            int width;
-            int height;
-            int.TryParse(cWidth.Value, out width);
-            int.TryParse(cHeight.Value, out height);
-            _goToScreen(new MainViewModel(width, height));
+            _goToScreen(new MainViewModel(cWidth.Value, cHeight.Value, cName1.Value, cName2.Value));
         }
     }
 }
